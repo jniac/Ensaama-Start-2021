ffbe110facb966ab793e57d8a3b4678f265ac65f
Joseph Merdrignac <joseph.merdrignac@gmail.com>
2021-09-23 12:36:22 +0200

    eye update

---
+++ Assets/Eye.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    public Transform target;
+
+    void Update() {
+        if (target != null) {
+            transform.LookAt(target);
+        }
+    }
+}
